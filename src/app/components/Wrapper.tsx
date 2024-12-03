f5c226f414d150bb01c28a909a5c4745f509a41c
jegedeglory <jegsboy007@gmail.com>
2024-12-03 20:43:24 +0100

    sidebar design

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import Image from "next/image";
 import Section from "./Section";
 
-type Props = {};
+type Props = {
+
+};
 
 const Wrapper = (props: Props) => {
   return (
     <div>
-      <div className="sidebar">
+      <div className="">
         <Section 
-        notTop
+    notTop
         >
 
 
-        <p className="fs-20 ">
+        <p className="fs-20 my-2">
             New Me
             </p>
         </Section>
