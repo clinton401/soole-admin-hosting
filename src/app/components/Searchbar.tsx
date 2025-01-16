885bf46dec88b3ba033617ebfcef0a2327adbc46
jegedeglory <jegsboy007@gmail.com>
2025-01-16 11:01:06 +0100

    fix: petty adjustments

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -9,32 +9,41 @@ const Searchbar = ({ isVisible = true }: Props) => {
   if (!isVisible) return null;
 
   return (
-    <div className="" style={{width: "100%"}}>
+    <div className="" style={{ width: "100%" }}>
       <div className="header ">
         <div className="search-bar">
           <input type="text" placeholder="Search" />
         </div>
         <div className="user-settings">
           <div className="notify mx-1">
-            <div className="notification"></div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
-  <path d="M10.1074 1.30153L10.1074 1.30153L10.2784 1.37747C10.2784 1.37748 10.2784 1.37748 10.2784 1.37748C10.293 1.38395 10.3075 1.39048 10.322 1.39707C9.54095 2.03091 9.04163 2.9984 9.04163 4.08335C9.04163 5.99334 10.59 7.54168 12.5 7.54168C12.7291 7.54168 12.9533 7.51932 13.1704 7.47662C13.2563 8.16341 13.4542 8.83405 13.7579 9.46331C13.7579 9.46334 13.7579 9.46336 13.7579 9.46339L13.979 9.92181L13.979 9.92184C14.7644 11.5497 13.7608 13.5053 11.8419 13.8463C11.8419 13.8463 11.8418 13.8463 11.8418 13.8464L11.7081 13.8701L11.7079 13.8701C8.85721 14.3766 5.93387 14.3766 3.0832 13.8701L3.08318 13.8701C1.15043 13.5268 0.196695 11.5089 1.09055 9.92073L1.27971 9.58471L1.27975 9.58464C1.80937 8.64361 2.08775 7.58849 2.08775 6.51493V5.45357C2.08775 3.73922 3.09752 2.1544 4.72444 1.37616L4.72445 1.37616C6.41309 0.568382 8.39463 0.540724 10.1074 1.30153Z" stroke="black" strokeWidth="1.5"/>
+            <div className="notification"><p className="">4</p></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M13.3153 20.029L13.3155 20.029C13.6061 19.9363 13.8685 19.8009 14.0883 19.6317C14.2354 19.5183 14.3611 19.3916 14.4631 19.2551C14.0157 19.2962 13.0736 19.3746 12.3969 19.3746C11.7202 19.3746 10.778 19.2962 10.3306 19.2551C10.4326 19.3915 10.5583 19.5183 10.7056 19.6318L13.3153 20.029ZM13.3153 20.029C13.0248 20.1218 12.7126 20.1697 12.3969 20.1697C12.0811 20.1697 11.769 20.1218 11.4784 20.029L11.4782 20.029M13.3153 20.029L11.4782 20.029M11.4782 20.029C11.1877 19.9363 10.9254 19.801 10.7057 19.6319L11.4782 20.029Z" stroke="black" strokeWidth="1.5"/>
+  <path d="M15.1074 4.30153L15.1074 4.30153L15.2784 4.37747C15.2784 4.37748 15.2784 4.37748 15.2784 4.37748C15.293 4.38395 15.3075 4.39048 15.322 4.39707C14.541 5.03091 14.0416 5.9984 14.0416 7.08335C14.0416 8.99334 15.59 10.5417 17.5 10.5417C17.7291 10.5417 17.9533 10.5193 18.1704 10.4766C18.2563 11.1634 18.4542 11.834 18.7579 12.4633C18.7579 12.4633 18.7579 12.4634 18.7579 12.4634L18.979 12.9218L18.979 12.9218C19.7644 14.5497 18.7608 16.5053 16.8419 16.8463C16.8419 16.8463 16.8418 16.8463 16.8418 16.8464L16.7081 16.8701L16.7079 16.8701C13.8572 17.3766 10.9339 17.3766 8.0832 16.8701L8.08318 16.8701C6.15043 16.5268 5.19669 14.5089 6.09055 12.9207L6.27971 12.5847L6.27975 12.5846C6.80937 11.6436 7.08775 10.5885 7.08775 9.51493V8.45357C7.08775 6.73922 8.09752 5.1544 9.72444 4.37616L9.72445 4.37616C11.4131 3.56838 13.3946 3.54072 15.1074 4.30153Z" stroke="black" strokeWidth="1.5"/>
 </svg>
           </div>
-      <Image
-        className="user-img"
-        // src="https://images.unsplash.com/photo-1587918842454-870dbd18261a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=943&q=80"
-        width={40}
-        height={40}
-        objectFit="cover"
-        src="/profilePic.png"
-        alt=""
-      />
-      <div>
-
-          <div className="user-name ff-Mabry-Pro-semibold fs-16" style={{width: "140px"}}>Adeyemi Korede</div>
-          <div className="user-name ff-Mabry-Pro-light fs-16 card-title" style={{color: "#"}}>Admin</div>
-      </div>
+          <Image
+            className="user-img"
+            width={40}
+            height={40}
+            objectFit="cover"
+            src="/profilePic.png"
+            alt=""
+          />
+          <div>
+            <div
+              className="user-name ff-Mabry-Pro-semibold fs-16"
+              style={{ width: "140px" }}
+            >
+              Adeyemi Korede
+            </div>
+            <div
+              className="user-name ff-Mabry-Pro-semibold fs-16 card-title"
+              style={{ color: "#" }}
+            >
+              Admin
+            </div>
+          </div>
         </div>
       </div>
     </div>
