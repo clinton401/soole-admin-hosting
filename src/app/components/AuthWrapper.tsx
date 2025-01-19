af404ca34573e1eb8bb82bad8af8d1d80a4974a5
jegedeglory <jegsboy007@gmail.com>
2025-01-19 14:47:29 +0100

    Auth page

--- src/app/components/AuthWrapper.tsx
+++ src/app/components/AuthWrapper.tsx
@@ -15,3 +15,47 @@ export default function AuthWrapper({ children }: { children: React.ReactNode })
 
   return <>{children}</>;
 }
+
+
+
+
+
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function AuthWrapper({ children }: { children: React.ReactNode }) {
+//   const router = useRouter();
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   useEffect(() => {
+//     const checkAuthStatus = async () => {
+//       try {
+//         // Call backend API to verify authentication status
+//         const response = await fetch("/api/auth/verify", {
+//           method: "GET",
+//           credentials: "include", // Sends cookies with the request
+//         });
+
+//         if (response.ok) {
+//           const data = await response.json();
+//           setIsAuthenticated(data.isAuthenticated);
+//         } else {
+//           router.push("/auth/login");
+//         }
+//       } catch (error) {
+//         console.error("Failed to verify authentication:", error);
+//         router.push("/auth/login");
+//       }
+//     };
+
+//     checkAuthStatus();
+//   }, [router]);
+
+//   if (!isAuthenticated) {
+//     return null; // Optionally, you can add a loading spinner here.
+//   }
+
+//   return <>{children}</>;
+// }
