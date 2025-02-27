1532e9144d23031e49a6aa69b52d256a4fe832b0
jegedeglory <jegsboy007@gmail.com>
2025-02-27 10:59:32 +0100

    Fix: useToast

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,34 +1,34 @@
 import { toast, Bounce } from "react-toastify";
 
-
-const useToast = (text: string, type: "info" | "success" | "warn" | "error" | "default" = "default"): void => {
-    if (type === "default") {
-        toast(text, {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: false,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-            transition: Bounce,
-        });
-        return;
-    }
-    toast[type](text, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
+const showToast = (
+  text: string,
+  type: "info" | "success" | "warn" | "error" | "default" = "default"
+): void => {
+  if (type === "default") {
+    toast(text, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
     });
+    return;
+  }
+  toast[type](text, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  });
+};
 
-}
-
-
-export default useToast;
+export default showToast;
