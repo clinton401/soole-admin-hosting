f94432f96814af6375f8a5d491386d0ca796bcb0
clinton <clintonphillips464@gmail.com>
2025-03-18 00:23:30 +0100

    fixed vercel last build error

--- src/app/components/InboxManager.tsx
+++ src/app/components/InboxManager.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import InboxList from "./InboxList";
-import InboxContent from "./InboxContent";
+// import InboxList from "./InboxList";
+// import InboxContent from "./InboxContent";
 
 const InboxManager = () => {
   const totalMessages = 100; // Total messages
@@ -22,14 +22,14 @@ const InboxManager = () => {
 
   return (
     <div className="inbox-manager">
-      <InboxList
+      {/* <InboxList
         activeMenu={activeMenu}
         onMenuChange={handleMenuChange}
         starredCount={starredCount}
-      />
-      {activeMenu === "All Inbox" && (
+      /> */}
+      {/* {activeMenu === "All Inbox" && (
         <InboxContent starred={starred} onStarToggle={handleStarToggle} />
-      )}
+      )} */}
       {activeMenu === "Sent" && <div>Sent Content Component</div>}
       {activeMenu === "Bin" && <div>Bin Content Component</div>}
     </div>
